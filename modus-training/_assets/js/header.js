674b50346225fc8aad42ed7e934cc51b4c0e477f
Furkan Dize <dizefurkan@gmail.com>
2017-06-09 18:06:37 +0300

    Search display none olayı

--- modus-training/_assets/js/header.js
+++ modus-training/_assets/js/header.js
@@ -1,1 +1,12 @@
-header.js
+function searcharea(){
+  var x = document.getElementById('searchbutton');
+  if(x.focus){
+    
+    x.style.display = 'block';
+    x.focus();
+  }
+  else{
+    x.style.display = 'none';
+
+  }
+}
